f8506248924119849061ac65c1ed0ac9b3a635f5
jiseung.jung <jiseung.jung@corretto.io>
2025-10-06 21:49:19 +0900

    FEAT: INIT

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cs.s2s2.kr',
+  url: 'https://corretto-inc.github.io/s2s2',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
